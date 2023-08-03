55b2ce92a02d80af28a347258db29ba6166da380
frank-Mwangi <kungofrank@gmail.com>
2023-08-03 16:31:34 +0300

    Convert float to int

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import math
-
 """
 Type-annotated function to find floors of float inputs
 """
@@ -8,4 +6,4 @@ Type-annotated function to find floors of float inputs
 
 def floor(n: float) -> int:
     """Turn float into integer"""
-    return math.floor(n)
+    return int(n)
