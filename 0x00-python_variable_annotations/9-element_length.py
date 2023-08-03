ab7c6a927fa41472114e61cd4d88d6b59f302495
frank-Mwangi <kungofrank@gmail.com>
2023-08-03 23:40:48 +0300

    Duck types

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,6 +7,6 @@ Duck types
 """
 
 
-def element_length(lst: Iterable[Sequence]) -> list[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a tuple containing a sequence and an int"""
     return [(i, len(i)) for i in lst]
