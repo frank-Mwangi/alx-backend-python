b2a890712515bb1c19c147dc875b86f460eb7ec2
frank-Mwangi <kungofrank@gmail.com>
2023-08-08 12:49:23 +0300

    Return a simple async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Async coroutine that yields random numbers in range 10
+"""
+
+import asyncio
+import random
+from typing import Iterator
+
+
+async def async_generator() -> Iterator[float]:
+    """return a list of random numbers between 1 and 10"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,1 @@
+Asynchronous comprehensions in Python
